a03da3ba667315fada7b3c2742ece489c1bfa234
J-992 <75342062+J-992@users.noreply.github.com>
2024-07-18 23:23:46 -0400

    more updates

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -15,8 +15,7 @@ const About: React.FC = () => {
         Ontario.
       </p>
       <p>
-        I am passionate about writing codes and <br />
-        developing web applications to solve real-life challenges.
+        [PLACEHOLDER]
       </p>
     </AboutWrapper>
   );
